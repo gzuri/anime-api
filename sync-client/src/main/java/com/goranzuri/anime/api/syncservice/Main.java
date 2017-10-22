e96d6f8f405a216596035fbfe9c4f7a10b6fe5ba
Goran Zuri <goran.zuri@gmail.com>
2017-10-22 20:24:44 +0200

    Added sync-client

---
+++ sync-client/src/main/java/com/goranzuri/anime/api/syncservice/Main.java
@@ -0,0 +1,74 @@
+package com.goranzuri.anime.api.syncservice;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.gson.Gson;
+import org.apache.commons.cli.*;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+
+/**
+ * Created by gzuri on 22/10/2017.
+ */
+public class Main {
+    private static final String DIRECTORY_ARGUMENT = "dir";
+    private static final String STORAGE_ARGUMENT = "storage";
+    private static final String URL_ARGUMENT = "url";
+
+    private static Options prepareCommandLineOptions() {
+        Option option_directory = OptionBuilder.withArgName(DIRECTORY_ARGUMENT).hasArg().withDescription("source directory to search").create(DIRECTORY_ARGUMENT);
+        Option option_storage = OptionBuilder.withArgName(STORAGE_ARGUMENT).hasArg().withDescription("source directory to search").create(STORAGE_ARGUMENT);
+        Option option_url = OptionBuilder.withArgName(URL_ARGUMENT).hasArg().withDescription("source directory to search").create(URL_ARGUMENT);
+
+        Options options = new Options();
+        options.addOption(option_directory);
+        options.addOption(option_storage);
+        options.addOption(option_url);
+
+        return options;
+    }
+
+    public static void main(String... args) throws ParseException, IOException {
+        CommandLineParser parser = new GnuParser();
+        CommandLine commandLine;
+        Options options = Main.prepareCommandLineOptions();
+        SyncDriveReq syncDriveReq = new SyncDriveReq();
+        List<String> folderList = new ArrayList<>();
+
+        commandLine = parser.parse(options, args);
+
+        if (args.length < 2 || !commandLine.hasOption(DIRECTORY_ARGUMENT) || !commandLine.hasOption(STORAGE_ARGUMENT)){
+            // Main.printHelpPage(options);
+            return;
+        }
+
+
+        File parentFolder = new File(commandLine.getOptionValue(DIRECTORY_ARGUMENT).toString());
+        File[] folders = parentFolder.listFiles(File::isDirectory);
+        Arrays.sort(folders);
+
+        for (File folder: folders){
+            folderList.add(folder.getName());
+        }
+
+        syncDriveReq.setStorage(commandLine.getOptionValue(STORAGE_ARGUMENT).toString());
+        syncDriveReq.setNamesOnDisk(folderList);
+
+        Gson gson = new Gson();
+        String json = gson.toJson(syncDriveReq);
+
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpPost request = new HttpPost(commandLine.getOptionValue(URL_ARGUMENT).toString());
+        StringEntity params =new StringEntity(json);
+        request.addHeader("content-type", "application/json");
+        request.setEntity(params);
+        httpClient.execute(request);
+    }
+}
---
+++ sync-client/src/main/java/com/goranzuri/anime/api/syncservice/SyncDriveReq.java
@@ -0,0 +1,39 @@
+package com.goranzuri.anime.api.syncservice;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import java.util.List;
+
+/**
+ * Created by gzuri on 22/10/2017.
+ */
+public class SyncDriveReq {
+    @JsonProperty
+    @NotEmpty
+    List<String> namesOnDisk;
+
+    @JsonProperty
+    @NotEmpty
+    String storage;
+
+    public SyncDriveReq(){
+
+    }
+
+    public List<String> getNamesOnDisk() {
+        return namesOnDisk;
+    }
+
+    public void setNamesOnDisk(List<String> namesOnDisk) {
+        this.namesOnDisk = namesOnDisk;
+    }
+
+    public String getStorage() {
+        return storage;
+    }
+
+    public void setStorage(String storage) {
+        this.storage = storage;
+    }
+}
